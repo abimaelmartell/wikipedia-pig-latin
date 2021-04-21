79812864f34762cb6691097016395f7a4c1d9941
Abimael Martell <abimex@gmail.com>
2021-04-21 13:21:17 -0700

    add spects for requests and model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:article) {
+    Article.new(
+      title: 'Dog',
+      content: 'Doc Content'
+    )
+  }
+
+  describe '#itletay' do
+    it 'should return title into pig latin' do
+      expect(article.itletay).to eq("Ogday")
+    end
+  end
+
+  describe '#ontentcay' do
+    it 'should return content into pig latin' do
+      expect(article.ontentcay).to eq("Ocday Ontentcay")
+    end
+  end
 end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -11,12 +11,32 @@ RSpec.describe "Articles", type: :request do
       }
     }}
 
-    it "returns http success" do
-      expect_any_instance_of(WikipediaApi).to receive(:fetch_article).and_return(stub_response)
+    context "With article not created" do
+      it "should call api and create entry" do
+        expect_any_instance_of(WikipediaApi).to receive(:fetch_article).and_return(stub_response)
 
-      get "/wiki/#{article_id}"
+        expect {
+          get "/wiki/#{article_id}"
+        }.to change { Article.count }
 
-      expect(response).to have_http_status(:success)
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "With article created" do
+      before do
+        Article.create!(
+          title: "Pig Latin",
+          content: "test",
+          wikipedia_id: article_id
+        )
+      end
+
+      it "shouldnt call api" do
+        expect_any_instance_of(WikipediaApi).to_not receive(:fetch_article)
+        get "/wiki/#{article_id}"
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
